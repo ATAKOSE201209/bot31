87bf97824049ff07ab08abd7fedd2c2b522c9edc
Ata KÖSE <84375138+ATAKOSE201209@users.noreply.github.com>
2023-07-30 20:17:00 +0300

    Add files via upload

---
+++ tyui.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Merhaba nasıl yardımcı olabilirim?")
+@bot.command()
+async def nasilhapicilir(ctx):
+    await ctx.send("hapı ağıza attıktan sonra su iç işe yarar.")
+@bot.command()
+async def ilacsaatleri(ctx):
+    await ctx.send("ezanenin söylediği gibi telefon alarmı ayarla olur")
+@bot.command()
+async def makarnanasilyapilir(ctx):
+    await ctx.send("su kaşlanır tuz ve sızı yağ eklenir makarnalar içine konur,8 dk beklenir")
+@bot.command
+async def akillitelefonlaaramayapma(ctx):
+    await ctx.send("telefon uygulamasını aç.orda sayılar var oraya numaraları yaz")
+bot.run("MTEzNTI0OTA4NDk5NTg4MzEwOA.GFc3Wt.-XVQ0MhvqcwjZl6wrTLt-67dfp-8BlPPldMCy4")
